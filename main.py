55fc1fc29092a758ac55a9caf0bbdd03d81ff50f
Abi Vu <64785968+Abivu@users.noreply.github.com>
2023-09-05 12:46:09 -0500

    init Food class and update main accordingly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
+
 
 scr = Screen()
 scr.setup(width=600, height=600)
@@ -10,6 +12,7 @@ scr.tracer(0)
 
 
 snake = Snake()
+food = Food()
 
 
 scr.listen()
@@ -21,9 +24,17 @@ scr.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    snake.move()
-    time.sleep(0.1)
     scr.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        # print("nom nom nom")
+        food.refresh()
+
+
+
 
 
 
