c0f92e0597b2ef4a5f8914d458e2089ef85f215c
Abi Vu <64785968+Abivu@users.noreply.github.com>
2023-09-05 12:04:50 -0500

    listener event & keypress on snake move directions

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ scr.tracer(0)
 snake = Snake()
 
 
+scr.listen()
+scr.onkey(snake.up, "Up")
+scr.onkey(snake.down, "Down")
+scr.onkey(snake.left, "Left")
+scr.onkey(snake.right, "Right")
+
+
 game_is_on = True
 while game_is_on:
-    scr.update()
-    time.sleep(0.05)
-
     snake.move()
-
+    time.sleep(0.1)
+    scr.update()
 
 
 
