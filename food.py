dddcce0af73c0b3e5fc8260c8f1fd9669f375d82
Abi Vu <64785968+Abivu@users.noreply.github.com>
2023-09-06 09:09:08 -0500

    regenerate food and its size

--- food.py
+++ food.py
@@ -7,14 +7,14 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=1, stretch_wid=1)
         self.color("blue")
         self.speed("fastest")
         self.refresh()
         
 
     def refresh(self):
-        x_cor = random.randint(-280, 280)
-        y_cor = random.randint(-280, 280)
+        x_cor = random.randint(-260, 260)
+        y_cor = random.randint(-260, 260)
         self.goto(x_cor, y_cor)
 
