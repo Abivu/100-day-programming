a1658585cc959b6aeb21bfeb8100010d1ae7f683
Abi Vu <vu.ab.159@gmail.com>
2023-09-10 18:23:49 -0500

    Save high_score attr to txt for reuse

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -4,7 +4,8 @@ class ScoreBoard(Turtle):
     def __init__(self): 
         super().__init__()
         self.curr_score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -17,13 +18,11 @@ class ScoreBoard(Turtle):
         self.write(f"Score: {self.curr_score} - High score: {self.high_score}", move=False, align="center", font=("Arial", 20, "normal"))
 
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("Game Over", move=False, align="center", font=("Arial", 20, "normal"))
-
     def is_high_score(self):
         if self.curr_score > self.high_score:
             self.high_score = self.curr_score
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
             return True
         return False
     
