f58ecfd15875a44d234f5a5fa3a7bf0f0b5d9c00
Abi Vu <vu.ab.159@gmail.com>
2023-09-10 17:26:57 -0500

    refactor code to save highscore in 1 session

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -4,6 +4,7 @@ class ScoreBoard(Turtle):
     def __init__(self): 
         super().__init__()
         self.curr_score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -13,14 +14,26 @@ class ScoreBoard(Turtle):
     def score(self):
         self.goto(0, 240)
         self.clear()
-        self.write(f"Score: {self.curr_score}", move=False, align="center", font=("Arial", 20, "normal"))
+        self.write(f"Score: {self.curr_score} - High score: {self.high_score}", move=False, align="center", font=("Arial", 20, "normal"))
 
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", move=False, align="center", font=("Arial", 20, "normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", move=False, align="center", font=("Arial", 20, "normal"))
 
+    def is_high_score(self):
+        if self.curr_score > self.high_score:
+            self.high_score = self.curr_score
+            return True
+        return False
     
+    
+    def reset(self):
+        if self.is_high_score():
+            self.curr_score = 0
+            self.score()
+    
+
     def upgrade(self):
         self.curr_score += 1
         self.score()
