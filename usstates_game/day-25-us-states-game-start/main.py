4394bc74f81d5c9935fe9ff6d12853b1392899a1
Abi Vu <vu.ab.159@gmail.com>
2023-09-11 23:21:53 -0500

    Update as requirements

--- usstates_game/day-25-us-states-game-start/main.py
+++ usstates_game/day-25-us-states-game-start/main.py
@@ -22,23 +22,25 @@ turtle.shape(img)
 
 
 df = pd.read_csv(f"{current_working_directory}{path_to_files}/50_states.csv")
-game_is_on = True
-while game_is_on:
-    answer_state = screen.textinput(title="Guess the States", prompt="What's another state's name? ")
+list_of_states = df["state"].to_list()
+correct_guesses = []
+# print(list_of_states)
+while len(correct_guesses) < 50:
+    if len(correct_guesses) == 0:
+        title = "Guess the States"
+    else:
+        title = f"{len(correct_guesses)}/50 States Guessed."
+    answer_state = screen.textinput(title=title, prompt="What's another state's name? ")
     formated_answer_state = answer_state.title() # Format answer
 
+    if formated_answer_state in list_of_states:
+        correct_guesses.append(formated_answer_state)
+        x_cor = float(df[df["state"] == formated_answer_state]["x"])
+        y_cor = float(df[df["state"] == formated_answer_state]["y"])
+        pointy = Point()
+        pointy.name_the_state(x_cor, y_cor, formated_answer_state)
 
-    x_cor = float(df[df["state"] == formated_answer_state]["x"])
-    y_cor = float(df[df["state"] == formated_answer_state]["y"])
-
-    pointy = Point()
-    pointy.name_the_state(x_cor, y_cor, formated_answer_state)
-
-## TODO: How game handles misspelling/wrong answer (dup answers can be ignored for now)
-## TODO: How game ends
-## TODO: Update correct answer in the chatbox
 ## TODO: Review folder/file path
 ## TODO: Reorganize code
-
 
 turtle.mainloop()
