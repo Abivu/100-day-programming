f0476b022b22658b5dc7b021d108131ea5b68264
Abi Vu <vu.ab.159@gmail.com>
2023-09-19 23:29:59 -0500

    optimize code from solution

--- auto_birthday_wisher/main.py
+++ auto_birthday_wisher/main.py
@@ -8,30 +8,32 @@ import os
 
 #1: Search in birthdays.csv 
 df = pd.read_csv("./birthdays.csv")
-today = dt.datetime.now()
+# Compact data month and day into a tuple
+today = (dt.datetime.now().month, dt.datetime.now().day)
 
 # Loop through Dataframe using index attribute
-for i in df.index:
-    month = df.month[i]
-    day = df.day[i]
-    if today.month == month and today.day == day:
-        name = df.name[i]
-        email_add = df.email[i]
-    # Not yet covering multiple birthdays on the same day
-#2: Take the data of interest and replace in placeholders with the random template
-# To avoid hard coding and manually update the number of files in the file path
-file_count = len(os.listdir("./letter_templates/")) 
-choice = random.randint(1, file_count)
-# Randomly choose the letter template
-file_path = f"./letter_templates/letter_{choice}.txt"
-message = []
-with open( file_path, "r") as birthday_card:
-    content = birthday_card.readlines()
-    first_line = content[0].replace("[NAME]", name)
-    message.append(first_line)
-    for line in content[1:]:
-        message.append(line)
-    message = "".join(message)
+# Dict comprehension at its finest - key is a tuple and value is a row of data
+birthday_dic = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in df.iterrows()}
+
+if today in birthday_dic:
+    #2: Take the data of interest and replace in placeholders with the random template
+    # To avoid hard coding and manually update the number of files in the file path
+    file_count = len(os.listdir("./letter_templates/")) 
+    choice = random.randint(1, file_count)
+    # Randomly choose the letter template
+    file_path = f"./letter_templates/letter_{choice}.txt"
+    message = []
+
+    # TODO: Not yet covering multiple birthdays on the same day
+    birthday_person = birthday_dic[today]
+
+    with open( file_path, "r") as birthday_card:
+        content = birthday_card.readlines()
+        first_line = content[0].replace("[NAME]", birthday_person["name"])
+        message.append(first_line)
+        for line in content[1:]:
+            message.append(line)
+        message = "".join(message)
 
 #3: Connect to email & write script that if it satisfies the condition, an email would be sent out
 
@@ -43,6 +45,6 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.login(user=my_email, password=my_password)
     connection.sendmail(
         from_addr=my_email,
-        to_addrs=email_add,
-        msg=f"Subject: Happy Birthday {name}\n\n{message}"
+        to_addrs=birthday_person["email"],
+        msg=f"Subject: Happy Birthday {birthday_person['name']}\n\n{message}"
     )
