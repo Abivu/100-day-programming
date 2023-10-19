48dfe5a62879178cfa7966ee2af71cf2693a3f9e
Abi Vu <vu.ab.159@gmail.com>
2023-10-19 17:58:01 -0500

    minor change

--- template_jinja/main.py
+++ template_jinja/main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", blogs= blog_posts)
+    return render_template("index.html", blogs=blog_posts)
 
 
 @app.route('/post/<int:index>')
